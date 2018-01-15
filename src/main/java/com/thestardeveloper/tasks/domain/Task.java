29aa3f1f552ef5cec71e69119c4027ba189417b4
Bartek Maraszek <b.maraszek@gmail.com>
2018-01-15 20:21:11 +0000

    Configure H2 in-memory DB

--- src/main/java/com/thestardeveloper/tasks/domain/Task.java
+++ src/main/java/com/thestardeveloper/tasks/domain/Task.java
@@ -3,14 +3,18 @@ package com.thestardeveloper.tasks.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Task {
+    @Id
     private Long id;
     private String name;
     @JsonFormat(pattern = "dd/MM/yyyy")
