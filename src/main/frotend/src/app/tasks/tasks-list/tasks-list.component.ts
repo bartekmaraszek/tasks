69e453892508528cd3ed2d3597f08b8dd1c100b4
Bartek Maraszek <b.maraszek@gmail.com>
2018-01-16 21:03:06 +0000

    Refine the tasks-list component

--- src/main/frotend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frotend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -13,6 +13,19 @@ export class TasksListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.tasks.push( new Task(1, "Task 1", false, "07/08/2018"));
+    this.tasks.push( new Task(2, "Task 2", false, "07/08/2018"));
+    this.tasks.push( new Task(3, "Task 3", false, "08/08/2018"));
+    this.tasks.push( new Task(4, "Task 4", false, "08/08/2018"));
+    this.tasks.push( new Task(5, "Task 5", false, "09/08/2018"));
+  }
+
+  getDueDateLabel(task: Task) {
+    return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  onTaskChange(event, task) {
+    this.taskService.saveTask(task, event.target.checked).subscribe();
   }
 
 }
